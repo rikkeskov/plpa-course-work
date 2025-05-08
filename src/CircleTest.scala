d89e2dacdaab397af0354f7d52fc25ac96cbeae5
rikke <rsuvsreesg@gmail.com>
2025-05-08 22:50:40 +0200

    added first test draf

---
+++ src/CircleTest.scala
@@ -0,0 +1,14 @@
+import org.mockito.Mockito._
+import org.scalatest.flatspec.AnyFlatSpec
+import org.scalatest.matchers.should.Matchers
+
+import java.awt.Graphics2D
+
+class CircleTest extends AnyFlatSpec with Matchers {
+  val g: Graphics2D = mock(classOf[Graphics2D])
+  "Circle.draw" should "draw circle correctly" in {
+    val circle = new Circle("(0 0) 10")
+    circle.draw(new DrawContext(g))
+  }
+
+}
--- test/DrawObjectsTests.scala
+++
@@ -1,9 +0,0 @@
-import Circle.draw
-
-import org.scalatest.funsuite.AnyFunSuite
-
-class CircleTest extends AnyFunSuite:
-  val graphics = mock(classOf[Graphics])
-  test("Circle.sc") {
-    assert(Circle.draw())
-  }
