0cc3158cdc511163363a03620ab8676100aaf289
Rasmus Møller <rmoller@wastehero.io>
2025-05-14 12:07:18 +0200

    Added so BoundingBox can take 2 coordinates and order is irrelevant.

--- src/BoundingBox.scala
+++ src/BoundingBox.scala
@@ -9,9 +9,29 @@ class BoundingBox(text: String) extends DrawObject[Array[Int]] with Object with 
     case s => throw DrawException(s"Cant match arguments to $command: $s", this)
   }
 
+  private def leftAlignRectangleCoordinates(x1: Int, y1: Int, x2: Int, y2: Int): Array[Int] = {
+    var lowX: Int = x1
+    var lowY: Int = y1
+    var highX: Int = x2
+    var highY: Int = y2
+    if(lowX > highX){
+      lowX = lowX + highX
+      highX = lowX - highX
+      lowX = lowX - highX
+    }
+    if(lowY > highY){
+      lowY = lowY + highY
+      highY = lowY - highY
+      lowY = lowY - highY
+    }
+    Array[Int](lowX, lowY, highX, highY)
+  }
+
   override def draw(context: DrawContext): Unit = {
     val g = context.graphics
-    val shape: Shape = new Rectangle(arguments(0) * 10, arguments(1) * 10, (arguments(2) - arguments(0)) * 10, (arguments(3) - arguments(1)) * 10)
+
+    val coordinates: Array[Int] = leftAlignRectangleCoordinates(arguments(0),arguments(1), arguments(2), arguments(3))
+    val shape: Shape = new Rectangle(coordinates(0) * 10, coordinates(1) * 10, (coordinates(2) - coordinates(0)) * 10, (coordinates(3) - coordinates(1)) * 10)
 
     g.setColor(fill)
     g.fill(shape)
